147dc9464778ae1c9557a96e0d16aac764cb8871
16usa <rus16zar@gmail.com>
2026-09-18 19:02:45 +0000

    test Pump.fun mint in GENESIS

--- artifacts/api-server/genesis/public/config.js
+++ artifacts/api-server/genesis/public/config.js
@@ -1,9 +1,9 @@
 window.GENESIS_CONFIG = {
   tokenName: 'GENESIS',
   ticker: '$GENESIS',
-  contractAddress: '',
-  pumpUrl: '',
-  explorerUrl: '',
+  contractAddress: 'tkJgS6xo3PRh69anvxdLTZMaFSBU9mRoUuEQPJNpump',
+  pumpUrl: 'https://pump.fun/?outputCurrency=tkJgS6xo3PRh69anvxdLTZMaFSBU9mRoUuEQPJNpump',
+  explorerUrl: 'https://solscan.io/token/tkJgS6xo3PRh69anvxdLTZMaFSBU9mRoUuEQPJNpump',
   burnPercent: 100,
   creatorFeePercent: 0.30,
   totalFeesUsd: 0,
